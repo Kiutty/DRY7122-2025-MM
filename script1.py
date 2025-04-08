83c45995d85ea4759f3c1d9416d8cb17cdc1cfbe
Juakindelbloque <joa.miranda@duocuc.cl>
2025-04-08 01:36:57 +0000

    Evaluación N°1 DRY7122-2025

---
+++ script1.py
@@ -0,0 +1,4 @@
+integrante1=input("Nombre integrante 1: ")
+integrante2=input("Nombre integrante 2: ")
+
+print("Evaluación N°1 Programación y Redes Virtualizadas-" + str(integrante1) + " " + str(integrante2))
---
+++ script2.py
@@ -0,0 +1,6 @@
+Nombre= input("Cuál es su nombre? ")
+Apellido= input("¿Cuál es su apellido? ")
+Seccion= input("¿Cuál es el número de tu sección? ")
+Sede= input("¿En que sede de Duoc estudia? ")
+
+print("Hola " + Nombre + " " + Apellido + "," + " " + "tu sección es la " + Seccion + " " + "y estudias en la sede Duoc " + Sede)
